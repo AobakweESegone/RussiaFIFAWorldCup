f7ee9a0a4360b5d123a9f3505f000b42e6513df5
Aobakwe Segone <Aobakwe.Segone@gijima.com>
2018-07-09 20:01:34 +0200

    Initial Commit

---
+++ RussiaFIFAWorldCup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RussiaFIFAWorldCup
+//
+//  Created by Aobakwe Segone on 2018/07/09.
+//  Copyright © 2018 aobakwesegone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RussiaFIFAWorldCup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RussiaFIFAWorldCup
+//
+//  Created by Aobakwe Segone on 2018/07/09.
+//  Copyright © 2018 aobakwesegone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
